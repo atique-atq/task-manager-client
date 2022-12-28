861dd194efe4be2433a887d05b21aba005e2a43a
atique-atq <atique.atq@gmail.com>
2022-12-28 11:19:32 +0600

    added firebase config

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
 
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
@@ -7,84 +8,102 @@ import classes from "./Header.module.scss";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    // const history = useHistory();
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [size, setSize] = useState({
-        width: undefined,
-        height: undefined,
-    });
+  // const history = useHistory();
+  const { user, logOut } = useContext(AuthContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
-    useEffect(() => {
-        const handleResize = () => {
-            setSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
-        window.addEventListener("resize", handleResize);
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    useEffect(() => {
-        if (size.width > 768 && menuOpen) {
-            setMenuOpen(false);
-        }
-    }, [size.width, menuOpen]);
-
-    const menuToggleHandler = () => {
-        setMenuOpen((p) => !p);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     };
+    window.addEventListener("resize", handleResize);
 
-    // const ctaClickHandler = () => {
-    //     menuToggleHandler();
-    //     history.push("/page-cta");
-    // };
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    return (
-        <header className={classes.header}>
-            <div className={classes.header__content}>
-                <Link to="/" className={classes.header__content__logo}>
-                <span className="flex justify-center items-center">
-                <img src="https://homearcbd.com/wp-content/uploads/2022/11/HOME-ARC-BD-LOGO.svg" className="mr-3 h-16" alt="HomeArc Logo" />
-                <span> TaskTracer</span>
-                </span>
-             
-                </Link>
-                <nav
-                    className={`${classes.header__content__nav} ${
-                        menuOpen && size.width < 768 ? classes.isMenu : ""
-                    }`}
-                >
-                    <ul>
-                        <li>
-                            <Link to="/page-one" onClick={menuToggleHandler}>
-                                PageOne
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/page-two" onClick={menuToggleHandler}>
-                                PageTwo
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/page-three" onClick={menuToggleHandler}>
-                                PageThree
-                            </Link>
-                        </li>
-                    </ul>
-                    <button>CTA Page</button>
-                </nav>
-                <div className={classes.header__content__toggle}>
-                    {!menuOpen ? (
-                        <BiMenuAltRight onClick={menuToggleHandler} />
-                    ) : (
-                        <AiOutlineClose onClick={menuToggleHandler} />
-                    )}
-                </div>
-            </div>
-        </header>
-    );
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.header__content}>
+        <Link to="/" className={classes.header__content__logo}>
+          <span className="flex justify-center items-center">
+            <img
+              src="https://homearcbd.com/wp-content/uploads/2022/11/HOME-ARC-BD-LOGO.svg"
+              className="mr-3 h-16"
+              alt="HomeArc Logo"
+            />
+            <span> TaskTracer</span>
+          </span>
+        </Link>
+        <nav
+          className={`${classes.header__content__nav} ${
+            menuOpen && size.width < 768 ? classes.isMenu : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <Link to="/page-one" onClick={menuToggleHandler}>
+                Add Task
+              </Link>
+            </li>
+            <li>
+              <Link to="/page-two" onClick={menuToggleHandler}>
+                My Tasks
+              </Link>
+            </li>
+            <li>
+              <Link to="/page-three" onClick={menuToggleHandler}>
+                Completed Tasks
+              </Link>
+            </li>
+            {user?.uid ? (
+              <>
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+                <li>
+                  <button onClick={handleLogOut}>Sign out</button>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to="/">Login</Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+        <div className={classes.header__content__toggle}>
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
