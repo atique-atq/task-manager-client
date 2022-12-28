1ea3a9fbab637dea7b71f959d84ed77573282754
atique-atq <atique.atq@gmail.com>
2022-12-28 16:48:25 +0600

    imagebb added

--- src/pages/AddTask/AddTask.js
+++ src/pages/AddTask/AddTask.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../contexts/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const AddTask = () => {
   const { user, loading } = useContext(AuthContext);
@@ -9,12 +11,52 @@ const AddTask = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    defaultValues: {
-      category: "",
-      condition: "",
-    },
-  });
+  } = useForm();
+
+  const imageHostKey = process.env.REACT_APP_imgbb_key;
+  const navigate = useNavigate();
+
+  const handleAddTask = (data) => {
+    console.log("---", data);
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgData) => {
+        if (imgData.success) {
+          console.log(data);
+          const task = {
+            image: imgData.data.url,
+            description: data.description,
+            name: data.name,
+            createdEmail: user?.email,
+            status: "not completed",
+            creationTime: new Date(),
+          };
+
+          // save product information to the database
+          //   fetch("https://sharewear-server.vercel.app/products", {
+          //     method: "POST",
+          //     headers: {
+          //       "content-type": "application/json",
+          //       authorization: `bearer ${localStorage.getItem('accessToken')}`
+          //     },
+          //     body: JSON.stringify(product),
+          //   })
+          // .then((res) => res.json())
+          // .then((result) => {
+          //   console.log(result);
+          //   toast.success(`${data.name} is added successfully`);
+          //   navigate("/dashboard/myproducts");
+          // });
+        }
+      });
+  };
 
   return (
     <div>
@@ -22,7 +64,10 @@ const AddTask = () => {
         <h2 className="text-2xl text-[#0DA5E9] md:text-center text-left font-bold">
           Add a Task
         </h2>
-        <form className="border border-indigo-400 shadow-lg py-12 px-3 md:px-6 mt-3 mx-auto">
+        <form
+          onSubmit={handleSubmit(handleAddTask)}
+          className="border border-indigo-400 shadow-lg py-12 px-3 md:px-6 mt-3 mx-auto"
+        >
           <div>
             <div className="form-control w-full p-2 mb-3">
               <div className="flex input-bordered rounded-none">
@@ -58,11 +103,16 @@ const AddTask = () => {
                   rows={6}
                   type="text"
                   {...register("description", {
-                    required: false,
+                    required: "Description is Required",
                   })}
                   className="input input-bordered w-full rounded-none bg-slate-300 p-3"
                 />
               </div>
+              {errors.description && (
+                <p className="text-red-500 text-xs">
+                  {errors.description.message}
+                </p>
+              )}
             </div>
 
             <div className="form-control w-full p-2 mb-3">
@@ -89,7 +139,7 @@ const AddTask = () => {
             <div className="flex justify-center items-center">
               <input
                 className="bg-[#0DA5E9] p-3 md:w-80 w-64 rounded-md mt-1"
-                value="Add Product"
+                value="Add Task"
                 type="submit"
               />
             </div>
