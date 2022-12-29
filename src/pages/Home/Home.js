4c43578c320d26d8eca0296840c3bffa8d3a10ca
atique-atq <atique.atq@gmail.com>
2022-12-29 08:12:23 +0600

    showed all tasks

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,17 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <div
-      className=" dark:bg-slate-800 px-6 py-8 mx-12 ring-1 ring-slate-900/5 shadow-xl"
+      className=" dark:bg-slate-800 px-6 py-8 mx-12 ring-1 ring-slate-900/5 shadow-xl flex justify-center mt-6"
       style={{ minHeight: "100vh" }}
     >
-      <div></div>
-      <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
-        Writes Upside-Down
-      </h3>
-      <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-        The Zero Gravity Pen can be used to write in any orientation, including
-        upside-down. It even works in outer space.
-      </p>
+      <div>
+        <h1 className="text-slate-900 dark:text-white mt-5 text-3xl font-medium tracking-tight">
+          Welcome to Task Tracer.
+        </h1>
+        <h2 className="text-slate-500 dark:text-slate-400 mt-2 text-xl">
+          Task managements has become easier with this app. Trace all your to do
+          lists from the options above in the navbar.
+        </h2>
+      </div>
     </div>
   );
 };
--- src/pages/MyTasks/MyTasks.js
+++ src/pages/MyTasks/MyTasks.js
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 const MyTasks = () => {
   const { user, loading } = useContext(AuthContext);
 
-  const url = `http://localhost:5000/mytasks?email=${user?.email}`;
+  const url = `http://localhost:5000/alltasks`;
   const { data: myTasks, refetch } = useQuery({
     queryKey: ["myTasks"],
     queryFn: async () => {
@@ -17,28 +17,35 @@ const MyTasks = () => {
   console.log("---the tasks are:", myTasks);
   return (
     <div
-      className=" dark:bg-slate-800 px-6 py-8 mx-12 ring-1 ring-slate-900/5 shadow-xl"
+      className=" dark:bg-slate-800 px-2 md:px-6 py-8 mx-1 md:mx-12 ring-1 ring-slate-900/5 shadow-xl"
       style={{ minHeight: "100vh" }}
     >
       <div className="m-4">
-        <h1 className="text-2xl font-bold text-center my-4 pb-8 underline decoration-slate-200 underline-offset-4 font-mono text-[#F6740A]">
-          My Tasks list:
+        <h1 className="text-2xl font-bold text-center my-4 pb-8 underline decoration-slate-200 underline-offset-4 font-mono text-[#a2a2f6]">
+          Tasks List:
         </h1>
-        <div className="overflow-x-auto bg-slate-300">
-          <table className="table w-full">
+        <div className="overflow-x-auto bg-slate-500 rounded-lg">
+          <table className="border-separate my-table-spacing w-full ">
             <thead>
               <tr>
                 <th></th>
                 <th>Name</th>
                 <th>Description</th>
+                <th></th>
               </tr>
             </thead>
             <tbody className="text-center">
               {myTasks?.map((task, i) => (
-                <tr key={task._id}>
+                <tr className="mb-12" key={task._id}>
                   <th>{i + 1}</th>
                   <td>{task.name}</td>
                   <td>{`${task.description.substring(0, 100)}...`}</td>
+                  <td>
+                    <button className="bg-[rgb(162, 162, 246)] border-2 border-slate-400 py-0 px-3 rounded-lg text-md hover:bg-[#a2a2f6]">
+                      show details
+                    </button>
+                  </td>
+                  <br />
                 </tr>
               ))}
             </tbody>
